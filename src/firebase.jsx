37ead22c4de25f3a50aba2b7d2a134e08a41bdb3
Himanshu <himanshuparodiya@gmail.com>
2023-06-06 11:38:00 +0530

    setup firebase and redux

---
+++ src/firebase.jsx
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA-dUhMUX54-RPCs4llV5F8xQMNXRGHkes",
+    authDomain: "netflix-clone-d982c.firebaseapp.com",
+    projectId: "netflix-clone-d982c",
+    storageBucket: "netflix-clone-d982c.appspot.com",
+    messagingSenderId: "443548679126",
+    appId: "1:443548679126:web:c54aac5514309bf2bcc277"
+  };
