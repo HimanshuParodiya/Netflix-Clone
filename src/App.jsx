83378ea5f2ed944267723b8e1e15a42e4aaa2d0a
Himanshu <himanshuparodiya@gmail.com>
2023-06-07 14:39:15 +0530

    Set up router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import "./App.css"
 import HomeScreen from './components/HomeScreen'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 const App = () => {
   return (
     <div className='app'>
-      <HomeScreen />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<HomeScreen />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -20,7 +20,6 @@ const Banner = () => {
         fetchData()
     },[])
 
-    console.log(movie);
 
     // Function if the lenth of text is more then show .....
     const truncate = (string, n) =>{
