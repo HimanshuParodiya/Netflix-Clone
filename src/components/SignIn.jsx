acd6fdc9d52a536728cad30131f392ee3448d6b9
Himanshu <himanshuparodiya@gmail.com>
2023-06-08 22:37:12 +0530

    Add new user in firebase database

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,18 +1,39 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import "./SignIn.css"
+import { auth, createUserWithEmailAndPassword } from '../firebase/firebase'
 
 const SignIn = () => {
+    // getting email
+    const emailRef = useRef(null)
+    const passwordRef = useRef(null)
+
+    const register = (e) =>{
+        // e.preventDefault();
+        createUserWithEmailAndPassword(auth,
+            emailRef.current.value,
+            passwordRef.current.value
+        ).then((authUser) =>{
+            console.log(authUser);
+        }).catch((error)=>{
+            alert(error.message)
+        })
+    }
+    
+    const handleSignIn = (e) =>{
+        e.preventDefault();
+        
+    }
     return (
         <div className='signInScreen'>
             <form >
                 <h1>Sign In</h1>
-                <input type="email" placeholder='Email' />
-                <input type="password" placeholder='Password' />
-                <button type="submit">Sign In</button>
+                <input ref={emailRef} type="email" placeholder='Email' />
+                <input ref={passwordRef} type="password" placeholder='Password' />
+                <button type="submit" onClick={handleSignIn}>Sign In</button>
                 
                 <h4>
                     <span className="signInScreen_gray">New to Netflix? </span>
-                    <span className="signInScreen_link">Sign Up now.</span>
+                    <span className="signInScreen_link" onClick={register}>Sign Up now.</span>
                      
                 </h4>
             </form>
--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -1,3 +1,8 @@
+import {initializeApp} from 'firebase/app';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {getFirestore} from "firebase/firestore"
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyA-dUhMUX54-RPCs4llV5F8xQMNXRGHkes",
     authDomain: "netflix-clone-d982c.firebaseapp.com",
@@ -6,3 +11,12 @@ const firebaseConfig = {
     messagingSenderId: "443548679126",
     appId: "1:443548679126:web:c54aac5514309bf2bcc277"
   };
+
+  const firebaseApp = initializeApp(firebaseConfig)
+  const db = getFirestore(firebaseApp)
+  const auth = getAuth();
+
+  export {auth, createUserWithEmailAndPassword}
+  
+  
+  export default db;
